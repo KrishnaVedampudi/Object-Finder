2600a1d41f6ac073b2131c26e425575fbc3ae98b
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2021-06-25 19:17:09 +0530

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -1,4 +1,21 @@
+
 function Computer()
+{
+   window.location.replace("computer.html")
+}
+function Sports()
+{
+   window.location.replace("computer.html")
+}
+function Laptop()
+{
+   window.location.replace("computer.html")
+}
+function myStuff()
+{
+   window.location.replace("computer.html")
+}
+function BEDS()
 {
    window.location.replace("computer.html")
 }
